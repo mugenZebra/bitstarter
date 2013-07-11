2479ff046b0951c189763a9bd292c24f31b2c242
Andre Nguyen Van Qui <c087892275@gmail.com>
2013-07-11 01:53:57 +0000

    read from disk

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
-
+var buf = require('buffer');
+var fs = require('fs');
 var app = express.createServer(express.logger());
+var filename = "index.html";
+var buffer = new Buffer(fs.readFileSync(filename), "utf-8");
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
